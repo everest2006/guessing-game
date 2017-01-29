e18774f27575f31cf69fcbb0b93ddd77fae2112f
Yury Markouski <everest2006@tut.by>
2017-01-29 14:54:55 +0300

    the finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.MinValueOfRange = null;
+        this.MaxValueOfRange = null;
+        this.Candidate = null;
+    }
     setRange(min, max) {
-
+        this.MinValueOfRange = min;
+        this.MaxValueOfRange = max;
     }
-
     guess() {
-
+        this.Candidate = Math.ceil((this.MaxValueOfRange-this.MinValueOfRange)/2)+this.MinValueOfRange;
+        return this.Candidate;
     }
-
     lower() {
-
+        this.MaxValueOfRange = this.Candidate;
     }
-
     greater() {
-
+        this.MinValueOfRange = this.Candidate;
     }
 }
-
 module.exports = GuessingGame;
